bb0dd7b26804b4002d70992926ca2d6282ef6501
re1zo <marcin.marynowski@gmail.com>
2019-02-20 20:17:10 +0100

    Add TableView to Settings tab

--- TravelManager/Source/Settings/SettingsViewController.swift
+++ TravelManager/Source/Settings/SettingsViewController.swift
@@ -1,8 +1,27 @@
 import UIKit
 
 final class SettingsViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "defaultSettingsCell")
+    }
+}
+
+// MARK: - UITableViewDelegate && UITableViewDataSource
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "defaultSettingsCell") ?? UITableViewCell()
     }
 }
